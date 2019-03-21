25dbd493f0736a0124488a5b82cf661c25970f3e
Kuzzzya99 <34253792+Kuzzzya99@users.noreply.github.com>
2019-03-21 04:21:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var oc = "(", cc = ")", os = "[", cs = "]", l = "|", of = "{", cf = "}";
+    var ocCount = 0, osCount = 0, lCount = 0, ofCount = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == oc) {
+            ocCount++;
+        }
+        else if (str[i] == os) {
+            osCount++;
+        }
+        else if (str[i] == l) {
+            lCount++;
+        }
+        else if (str[i] == of) {
+            ofCount++;
+        }
+
+
+        else if (str[i] == cc) {
+            ocCount--;
+            if (ocCount < 0) {
+                return false;
+            }
+        }
+        else if (str[i] == cs) {
+
+            osCount--;
+            if (osCount < 0) {
+                return false;
+            }
+        }
+        else if (str[i] == cf) {
+            ofCount--;
+            if (ofCount < 0) {
+                return false;
+            }
+        }
+    }
+    if (osCount == 0 && ocCount == 0 && ofCount == 0 && (lCount % 2 == 0)) {
+        return true;
+    }
+    return false;
 }
