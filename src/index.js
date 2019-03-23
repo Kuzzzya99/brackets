e1ee0b6defdfcbe548d96324de5ff2690017b14b
Kuzzzya99 <34253792+Kuzzzya99@users.noreply.github.com>
2019-03-23 22:34:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-    var oc = "(", cc = ")", os = "[", cs = "]", l = "|", of = "{", cf = "}";
-    var ocCount = 0, osCount = 0, lCount = 0, ofCount = 0;
-
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] == oc) {
-            ocCount++;
-        }
-        else if (str[i] == os) {
-            osCount++;
-        }
-        else if (str[i] == l) {
-            lCount++;
-        }
-        else if (str[i] == of) {
-            ofCount++;
-        }
-
-
-        else if (str[i] == cc) {
-            ocCount--;
-            if (ocCount < 0) {
-                return false;
-            }
-        }
-        else if (str[i] == cs) {
-
-            osCount--;
-            if (osCount < 0) {
-                return false;
-            }
-        }
-        else if (str[i] == cf) {
-            ofCount--;
-            if (ofCount < 0) {
-                return false;
-            }
-        }
+ 
+  let string = str;
+  for (let j = 0; j < str.length; j++)
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let bracketsPair = bracketsConfig[i][0]+bracketsConfig[i][1];
+    while (true) {
+      if (string.includes(bracketsPair)) string = string.replace(bracketsPair, '')
+      else break;
     }
-    if (osCount == 0 && ocCount == 0 && ofCount == 0 && (lCount % 2 == 0)) {
-        return true;
-    }
-    return false;
-}
+  }
+  return (string.length === 0)
+  }
